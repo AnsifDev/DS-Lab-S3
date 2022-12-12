95173f7e1927b6fafb7557a4c8421bef03ac207f
AnsifDev <ansifmshamsu7025@gmail.com>
2022-12-12 23:34:20 +0530

    new file:   hashingSearch.c

---
+++ hashingSearch.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+int keys[10]; //Hash Key Table
+#define size 10 //Hash Key Table size
+
+int hash(int key) {
+	if (key == 0) return -1;
+
+	int index = 0;
+	for (int Num = key; Num != 0; Num /= size) index += Num%size;
+	index %= size;
+
+	index = index<0?index*-1:index;
+
+	int cIndex = index;
+	while (keys[cIndex] != 0) {
+		if (keys[cIndex] == key) return cIndex;
+		cIndex = (cIndex+1)%size;
+		if (cIndex == index) return -1;
+	}
+
+	return cIndex;
+}
+
+int put(int key) {
+	int index = hash(key);
+	if (index < 0) return index;
+	
+	if (keys[index] != 0) printf("Hash Key already exists!!!\n");
+	else {
+		keys[index] = key;
+		printf("Log: Generated index is %d\n", index);
+	}
+	return 0;
+}
+
+void inputKey() {
+	int key;
+	printf("Enter the key: ");
+	scanf("%d", &key);
+
+	int index = hash(key);
+	if (index < 0) printf("Looks like hash table is full or key maybe 0!!!\n");
+	else if (keys[index] != 0) printf("Hash Key already exists!!!\n");
+	else {
+		keys[index] = key;
+		printf("Log: Generated index is %d\n", index);
+	}
+}
+
+void searchKey() {
+	int key;
+	printf("Enter the key: ");
+	scanf("%d", &key);
+
+	int index = hash(key);
+	if (keys[index] != 0) printf("The index is %d\n", index);
+	else printf("Hash key not found!!!\n");
+}
+
+void main() {
+	printf("Menu:\n    1. Value Input\n    2. Search Key\n");
+	printf("    3. Exit\n");
+	
+	while(1) {
+		int input;
+		printf(">> ");
+		scanf("%d", &input);
+		
+		switch(input) {
+			case 1: inputKey(); break;
+			case 2: searchKey(); break;
+			case 3: printf("Exiting...\n"); return;
+			default: printf("Invalid Input!!!\n"); continue;
+		}
+
+		printf("Keys:");
+		for (int i = 0; i < size; i++) printf("\t%d", keys[i]);
+		printf("\n");
+	}
+}
